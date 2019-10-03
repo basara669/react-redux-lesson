cc588a1db8746970e90100cf19d3986010c0ea30
basara669 <vengeance.669@gmail.com>
2019-10-03 20:53:09 +0900

    create class components and functional Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
 
-class App extends Component {
-  render() {
-    // const greeting = "Hi,Tom";
-    // const dom = <h1 className="foo">{greeting}</h1>;
-    // return dom;
-    return (
-      <div>
-        <label htmlFor="bar">bar</label>
-        <input
-          type="text"
-          onClick={() => {
-            console.log("I am clicked");
-          }}
-        />
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     // const greeting = "Hi,Tom";
+//     // const dom = <h1 className="foo">{greeting}</h1>;
+//     // return dom;
+//     return (
+//       <div>
+//         <label htmlFor="bar">bar</label>
+//         <input
+//           type="text"
+//           onClick={() => {
+//             console.log("I am clicked");
+//           }}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  );
+};
+
+const Cat = () => {
+  return <div>Meow!</div>;
+};
 
 export default App;
