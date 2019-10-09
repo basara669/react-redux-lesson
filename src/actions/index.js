de2602f1cd30a48477d209ef3f8bc68f68f14fc4
basara669 <vengeance.669@gmail.com>
2019-10-09 12:49:20 +0900

    imprement action Creator

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+//複数で使うデータは一箇所で定義する
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
+
+//オブジェクトのこと
+//actionを返すものActionCreator
+
+export const increment = () => ({
+    type: INCREMENT
+});
+
+export const increment = () => ({
+    type: DECREMENT
+});
