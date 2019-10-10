36354d2f5c41d0ed00ada0ebbe7142c43468884b
basara669 <vengeance.669@gmail.com>
2019-10-10 12:48:51 +0900

    feat: useReducer

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,48 @@
 import * as React from "react";
-const { useState, useContext, createContext } = React;
+const { useContext, useReducer, createContext } = React;
 
-const MyContext = createContext(() => {});
+const DispatchContext = createContext(() => {});
 
-export const UseContextSample = () => {
-  const [count, setCount] = useState(0);
+const reducer = ({ year, month }, action) => {
+  switch (action) {
+    case "increment":
+      return month === 11
+        ? { year: year + 1, month: 0 }
+        : { year, month: month + 1 };
+    case "decrement":
+      return month === 0
+        ? { year: year - 1, month: 11 }
+        : { year, month: month - 1 };
+  }
+};
+
+export const UseReducerSample = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    year: 0,
+    month: 1
+  });
 
   return (
     <div>
       <p>
-        <b>{count}</b>
+        <b>
+          {state.year}年{state.month}ヶ月
+        </b>
       </p>
-      <MyContext.Provider value={() => setCount(count => count + 1)}>
-        <IncrementButton />
-      </MyContext.Provider>
+      <DispatchContext.Provider value={dispatch}>
+        <ControlButtons />
+      </DispatchContext.Provider>
     </div>
   );
 };
 
-const IncrementButton = () => {
-  const incrementHandler = useContext(MyContext);
+const ControlButtons = () => {
+  const dispatch = useContext(DispatchContext);
 
   return (
     <p>
-      <button onClick={incrementHandler}>+</button>
+      <button onClick={() => dispatch("decrement")}>-</button>
+      <button onClick={() => dispatch("increment")}>+</button>
     </p>
   );
 };
