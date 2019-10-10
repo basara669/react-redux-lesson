a2ae686a36e5b717c5cfe24556a4dee97d5dbd60
basara669 <vengeance.669@gmail.com>
2019-10-10 12:43:41 +0900

    add useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
 import * as React from "react";
-const { useState } = React;
+const { useState, useEffect } = React;
 
-export const UseStateSample = () => {
+export const UseEffectSample1 = () => {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setCount(count => count + 1);
+    }, 1000);
+    return () => clearTimeout(timerId);
+  }, [count]);
 
   return (
     <p>
-      <button onClick={() => setCount(count - 1)}>-</button>
-      <b>{count}</b>
-      <button onClick={() => setCount(count + 1)}>+</button>
+      time: <b>{count}</b>
     </p>
   );
 };
