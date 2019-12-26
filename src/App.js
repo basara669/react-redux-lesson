83032d355f0a4a82e467ba0e6a08591a2ff2757e
basara669 <vengeance.669@gmail.com>
2019-12-26 10:12:37 +0900

    modify app.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
+    //jsxはの内容はdomに入れることが出来る
     // const greeting = "Hi,Tom";
     // const dom = <h1 className="foo">{greeting}</h1>;
     // return dom;
-    return (
-      <div>
-        <label htmlFor="bar">bar</label>
-        <input
-          type="text"
-          onClick={() => {
-            console.log("I am clicked");
-          }}
-        />
-      </div>
-    );
+    //イベント周り
+    // return (
+    //  return は1つのタグで囲わないといけない
+    //  使いたくない場合は、React.Fragmenか<>を使用する
+    //   <div>
+    //    forもかぶってしまうため、htmlForを使用する
+    //     <label htmlFor="bar">bar</label>
+    //     <input
+    //       type="text"
+    //       onClick={() => {
+    //         console.log("I am clicked");
+    //       }}
+    //     />
+    //   </div>
+    // );
   }
 }
 
