623fa35dc5546a1cbf40a254566a69e80122f356
basara669 <vengeance.669@gmail.com>
2019-12-26 11:43:44 +0900

    modify files

--- src/components/App.js
+++ src/components/App.js
@@ -17,10 +17,14 @@ class App extends Component {
   }
 }
 
+// 本来なら親から子へ使わなきゃいけないものを、このように書くだけでどこでも使えるようにする
+// 前述の通りProviderでラップする必要がある
 const mapStateToProps = state => ({
   value: state.count.value
 });
 
+// 本来なら、親から子、更に孫などにイベント渡さないといけない
+// これを使用することで、どこでもイベントを発火させることができる
 // const mapDispatchToProps = dispatch => ({
 //   increment: () => dispatch(increment()),
 //   decrement: () => dispatch(decrement())
