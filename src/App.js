d0ab2319fd1ec2fed1e5b91d5bfa094cd6a925de
basara669 <vengeance.669@gmail.com>
2019-12-26 10:24:47 +0900

    modify app.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
-import React from "react";
+// Componentが消せる
+import React, { Component } from "react";
 
-// class App extends Component {
-//   render() {
-//     // const greeting = "Hi,Tom";
-//     // const dom = <h1 className="foo">{greeting}</h1>;
-//     // return dom;
-//     return (
-//       <div>
-//         <label htmlFor="bar">bar</label>
-//         <input
-//           type="text"
-//           onClick={() => {
-//             console.log("I am clicked");
-//           }}
-//         />
-//       </div>
-//     );
-//   }
-// }
+//先程のまでのがクラスコンポーネント
+class App extends Component {
+  render() {
+    // const greeting = "Hi,Tom";
+    // const dom = <h1 className="foo">{greeting}</h1>;
+    // return dom;
+    return (
+      <div>
+        <label htmlFor="bar">bar</label>
+        <input
+          type="text"
+          onClick={() => {
+            console.log("I am clicked");
+          }}
+        />
+      </div>
+    );
+  }
+}
 
-const App = () => {
-  return (
-    <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-    </div>
-  );
-};
+// const App = () => {
+//   return (
+//     <div>
+//       <Cat />
+//       <Cat />
+//       <Cat />
+//       <Cat />
+//     </div>
+//   );
+// };
 
-const Cat = () => {
-  return <div>Meow!</div>;
-};
+// const Cat = () => {
+//   return <div>Meow!</div>;
+// };
 
 export default App;
